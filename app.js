588ec991195409dec24c13ef6712552cf3e3f97f
Victoria Pope <vpope94@gmail.com>
2015-08-18 18:25:26 -0500

    added in loop that prints out a random amount of strings in the array

--- app.js
+++ app.js
@@ -1,21 +1,16 @@
 var app = angular.module('myApp', []);
 
-app.controller('basicController', function($scope) {
-    $scope.test = function () {
-        return $scope.x(2);
-    };
-    
+app.controller('basicController', function ($scope) {
+
     $scope.myMethod = function () {
-        return $scope.x(10 - 9).q();
-    };
-    
-    $scope.x = function (boo) {
-        if (boo === 1) {
-            return {
-                q: $scope.test
-            };
-        } else if (boo === 2) {
-            return 5 + boo;
+        var things = ['a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'];
+        var i;
+        function randomLoop () {
+            for (i = 0; i < things.length; i = i + 1) {
+                console.log(things[i]);
+            }
         }
+        randomLoop(Math.random() * 10);
     };
+
 });
