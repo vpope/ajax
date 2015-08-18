f451123b066a28cd174003ccbaf5c32c1a7356d0
Victoria Pope <vpope94@gmail.com>
2015-08-18 17:41:06 -0500

    created controller

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-var app = angular.module("myApp", []);
+var app = angular.module('myApp', []);
+
+app.controller('basicController', function($scope) {
+    $scope.test = function () {
+        return $scope.x(2);
+    };
+    
+    $scope.myMethod = function () {
+        return $scope.x(10 - 9).q();
+    };
+    
+    $scope.x = function (boo) {
+        if (boo === 1) {
+            return {
+                q: $scope.test
+            };
+        } else if (boo === 2) {
+            return 5 + boo;
+        }
+    };
+});
